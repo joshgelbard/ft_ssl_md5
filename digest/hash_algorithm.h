48d2f53d53dc2b6e0815bae478fc2e48e47bf287
Joshua Gelbard <gelbard.joshua@gmail.com>
2018-10-21 20:57:24 -0700

    Rename and norm in hash_algorithm.h

--- digest/hash_algorithm.h
+++ digest/hash_algorithm.h
@@ -1,19 +1,19 @@
-#ifndef HASH_ALGORITHM_CLASS_H
-# define HASH_ALGORITHM_CLASS_H
+#ifndef HASH_ALGORITHM_H
+# define HASH_ALGORITHM_H
 # include <string.h>
 
-struct s_hash_ctx;
+struct	s_hash_ctx;
 
-struct s_hash_algorithm
+struct	s_hash_algorithm
 {
-	int is_big_endian;
-	void (*finish_initialize)(struct s_hash_ctx *this);
-	void (*process_block)(struct s_hash_ctx *this);
-	char *(*to_string)(struct s_hash_ctx *this);
-	size_t block_size;
-	size_t word_size;
-	size_t digest_size;
-	char *algorithm_name;
-	int algorithm_id;
+	int		is_big_endian;
+	void	(*finish_initialize)(struct s_hash_ctx *ctx);
+	void	(*process_block)(struct s_hash_ctx *ctx);
+	char	*(*to_string)(struct s_hash_ctx *ctx);
+	size_t	block_size;
+	size_t	word_size;
+	size_t	digest_size;
+	char	*algorithm_name;
+	int		algorithm_id;
 };
 #endif
